8507497e789d02cfe9c16fc20560069bd011a3f9
Toon Tran <tatut90@gmail.com>
2023-01-14 17:09:11 +0700

    _FE_ setup assets n basic routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./index.css";
+import "./styles/icons/icons.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
 
