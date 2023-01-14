161cce1cd0f0302c9c98c53f40e36452ef89501e
Toon Tran <tatut90@gmail.com>
2023-01-14 17:39:41 +0700

    _FE_4-2 setup Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import rootReducer from "./reducers";
+// const store = createStore(rootReducer, composeWithDevTools());
+const store = configureStore({ reducer: rootReducer });
+
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
     </React.StrictMode>
 );
 
