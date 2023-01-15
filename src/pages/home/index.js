3b9e95e61f3c98a5939680960a07df50056f8561
Tuan Tran <tatut90@gmail.com>
2023-01-15 20:51:36 +0700

    FE-header User-Search-All menus (#1)
    * FE-header search menu
    
    * FE-header All Menu
    
    * FE-header User Menu

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,9 @@
+import Header from "../../comps/header";
+
 export default function Home() {
-  return <div>Home</div>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
